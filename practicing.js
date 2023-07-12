bcbeb55a14e455aff01110e56cb5a767d2142bcd
Jesus Perdomo <Japerdomo08@gmail.com>
2023-07-12 19:14:21 -0300

    First Commit

---
+++ practicing.js
@@ -0,0 +1,4 @@
+// Actividad 1
+// Escribe un codigo que recorra los números del 1 al 10 usando un bucle for. Por cada número, verifica si es par o
+// impar utilizando una estructura condicional if. Imprime un mensaje indicando si el número es par o impar.
+
